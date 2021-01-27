3cadd018759344991c77e2aa86b8965ed0271789
Victor Tuda <victor_gtuda@outlook.com>
2021-01-27 00:05:17 -0300

    -Add file main.py     -Add file words.pkl     -Add file classes.pkl -Alter file training.py

---
+++ main.py
@@ -0,0 +1,43 @@
+import random
+import json
+import pickle
+import numpy as np
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+from tensorflow.keras.models import load_model
+
+lemmatizer = WordNetLemmatizer()
+intents = json.loads(open('./intents.json').read())
+
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+model = load_model('chatbot_model.model')
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+    return sentence_words
+
+def bag_of_words(sentence):
+    setence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+
+    for w in setence_words:
+        for i, word in enumerate(words):
+            if word == w:
+                bag[i] = 1
+    return np.array(bag)
+
+def predict_class(sentence):
+    bow = bag_of_words(sentence)
+    res = model.predict(np.array([bow]))[0]
+    ERROR_TRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_TRESHOLD]
+
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({'intent': classes[r[0]], 'probability':str(r[1])})
+    return return_list
--- training.py
+++ training.py
@@ -5,6 +5,7 @@ import numpy as np
 
 import nltk
 nltk.download('punkt')
+nltk.download('wordnet')
 from nltk.stem import WordNetLemmatizer
 
 from tensorflow.keras.models import Sequential
@@ -66,7 +67,7 @@ model.add(Dense(len(train_y[0]), activation='softmax'))
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
-model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
-model.save('chatbot_model.model')
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.model.h5', hist)
 
 print('Done')
